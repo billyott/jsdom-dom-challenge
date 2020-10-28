cc785b7e009258dc04418ca758a9ef7e73d0e30e
billyott <willrichott@gmail.com>
2020-10-28 15:47:32 -0400

    got pause button working

--- js/challenge.js
+++ js/challenge.js
@@ -1,47 +1,69 @@
+//variables
 const counter = document.querySelector("#counter")
 counter.innerHTML = 0
-
 let counterContents = 0
-
-setInterval(function(){ 
-        counterContents++
-        counter.innerHTML = counterContents
-    ; }, 3000);
-    
+let pausedStatus = false
 const minusButton = document.querySelector("#minus")
-
-minusButton.addEventListener('click', handleDecrementCounter)
-
-function handleDecrementCounter() {
-    counterContents--
-    counter.innerHTML = counterContents
-}
-
 const plusButton = document.querySelector("#plus")
-plusButton.addEventListener('click', handleIncrementCounter)
-
-function handleIncrementCounter() {
-    counterContents++
-    counter.innerHTML = counterContents
-}
-
 const likesUl = document.querySelector(".likes")
 const heartButton = document.querySelector("#heart")
+const pauseButton = document.querySelector("#pause")
 
-heartButton.addEventListener('click', handleLikeRecord)
+console.log(pausedStatus)
 
-function handleLikeRecord() {
-    if (document.querySelector(`[data-counter-number='${counter.textContent}']`)) {
-        const counterLikeLi = document.querySelector(`[data-counter-number='${counter.textContent}']`)
-        counterLikeLi.textContent = `number: ${counterLikeLi.dataset.counterNumber} - like count: ${parseInt(counterLikeLi.dataset.counterNumberlikes)+1}`
-        counterLikeLi.dataset.counterNumberlikes = parseInt(counterLikeLi.dataset.counterNumberlikes)+1
-    } else {
-        const counterNumber = counter.innerHTML
-        const likeCount = 1
-        const counterLikeLi = document.createElement("li")
-        counterLikeLi.textContent = `number: ${counterNumber} - like count: ${likeCount}`
-        counterLikeLi.dataset.counterNumber = counterNumber
-        counterLikeLi.dataset.counterNumberlikes = likeCount
-        likesUl.append(counterLikeLi)
+//event handlers
+    
+setInterval(function() {
+    if (!pausedStatus) {
+        counterContents++
+        counter.innerHTML = counterContents;
+        }
+    }, 1000);
+
+function handleDecrementCounter() {
+    if (!pausedStatus) {
+    counterContents--
+    counter.innerHTML = counterContents
     }
 }
+
+function handleIncrementCounter() {
+    if (!pausedStatus) {
+    counterContents++
+    counter.innerHTML = counterContents
+    }
+}
+
+function handleLikeRecord() {
+    if (!pausedStatus) {
+        if (document.querySelector(`[data-counter-number='${counter.textContent}']`)) {
+            const counterLikeLi = document.querySelector(`[data-counter-number='${counter.textContent}']`)
+            counterLikeLi.textContent = `number: ${counterLikeLi.dataset.counterNumber} - like count: ${parseInt(counterLikeLi.dataset.counterNumberlikes)+1}`
+            counterLikeLi.dataset.counterNumberlikes = parseInt(counterLikeLi.dataset.counterNumberlikes)+1
+        } else {
+            const counterNumber = counter.innerHTML
+            const likeCount = 1
+            const counterLikeLi = document.createElement("li")
+            counterLikeLi.textContent = `number: ${counterNumber} - like count: ${likeCount}`
+            counterLikeLi.dataset.counterNumber = counterNumber
+            counterLikeLi.dataset.counterNumberlikes = likeCount
+            likesUl.append(counterLikeLi)
+        }
+    }
+}
+
+//event listeners
+minusButton.addEventListener('click', handleDecrementCounter)
+plusButton.addEventListener('click', handleIncrementCounter)
+heartButton.addEventListener('click', handleLikeRecord)
+pauseButton.addEventListener('click', function() {
+    if (!pausedStatus) {
+        pausedStatus = true
+        pauseButton.innerHTML = "resume"
+    } else {
+        pausedStatus = false
+        pauseButton.innerHTML = "pause"
+        counter.innerHTML = 0
+        counterContents = 0
+    }
+})
